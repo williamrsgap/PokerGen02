779cb5994937464a3ba7f31273d991f3f4fe2a4f
williamrsgap <146869255+williamrsgap@users.noreply.github.com>
2023-12-05 13:08:10 -0500

    Add files via upload

--- poker_gen_02.py
+++ poker_gen_02.py
@@ -2,10 +2,10 @@ import random, os
     
 class Mister_House:
     class Player:
-        def __init__(self):
-            self.name = "Courier"
+        def __init__(self,name,money=0):
+            self.name = name
             self.folded = False
-            self.money = 0
+            self.money = money
             self.hand = [[None,None],[None,None]]
 
         # raise (force raise for initial bet)
@@ -27,69 +27,19 @@ class Mister_House:
             pass
     
     class user(Player):
-        #user
+        #user   
         def request_action(self,house):
             if house.bet == 0:
-                req_init_bet()
+                self.req_init_bet()
             else:
-                req_play()
+                self.req_play()
         def req_init_bet(self,house):
             #ask the player for an initial bet through Maestro Class
             pass
         def req_play(self,house):
             #ask the player to raise, call, or fold
             pass
-            
-    class Maestro:
-        # orecstrate all ui
-        class Page:
-            def __init__(self,name: str,desc: str,ops):
-                self.name = name # page title (string)
-                self.desc = desc # description (string)
-                self.ops = ops # user options ([[option name,function]])
-        class Game(Page):
-            pass
-        
-        def __init__(self):
-            self.uni_cmds = {
-                "help":self.help,
-                "quit":exit
-            }
-            self.Main = Page("Main Menu","The starting menu",{
-                "play":[self.new_page,self.game_start],
-                "statistics":[self.new_page,self.stats]})
-            self.stats = Page("Statistics","Play Stats",{
-                "back":[self.new_page,self.Main]})
-            self.game_start = Game("Setup","Declare starting money and ai count",{})
-            self.active_page = self.Main
-        
-        def new_page(self,move_to: Page): #load new page and get next move
-            os.system('clr')
-            self.active_page = move_to
-            print(move_to.name+"\n\n\n\n")
-            print("Options\n\n")
-            for i in move_to.ops:
-                print(str.capitalize(i)+" | ",end="")
-            while(True):
-                nextm = input("Select an option:\n")
-                try:
-                    temp = move_to.ops[nextm]
-                    temp[0](temp[1])
-                    break
-                except:
-                    try:
-                        return self.uni_cmds[str.lower(nextm)]()
-                    except:
-                        print("Invalid Option, Try Again\n\n")
-                    continue
-        def help(self,page):
-            print("NAME:  "+page.name+"\n")
-            print("DESCRIPTION:  "+page.desc+"\n")
-            print("Options\n")
-            for i in move_to.ops:
-                print(str.capitalize(i)+" | ",end="")
-            print("\n\n\n")
-                
+          
     def __init__(self):
         self.players = []  # all player
         self.bank = 0 # house monet=y
@@ -100,6 +50,115 @@ class Mister_House:
         self.cards_flipped = [False] * 5 # which cards are face up 
         self.deck = [None]*52 # the current deck
         self.deck_place = 0 # which card is on top
+        
+        
+    
+    def setup(self,bank_value,players):
+        self.players = players
+        self.bank = bank_value
+        self.max_turn = len(players) -1
+
+    
+    def draw(self):
+    #draw top card
+        temp = self.deck[self.deck_place]
+        self.deck_place+=1
+        return temp
+
+    def create_deck(self):
+    # create an empty deck
+        deck = [] 
+        # iterate through each card
+        for i in range(len(self.SUIT)):
+            for j in range(len(self.TYPE)):
+                deck[i*14+j] = [i,j]
+        # give it out
+        return random.shuffle(deck)
+        
+    def deal(self):
+        # shuffle
+        self.deck = self.create_deck()
+        # dealt to players
+        for i in self.players:
+            i.hand = [self.draw(),self.draw()]
+        # placed on table
+        for i in self.table_cards:
+            i = self.draw()
+        # flip first three
+        self.cards_flipped = [True,True,True,False,False]
+    
+    def all_turn(self):
+        for i in self.players:
+            if i.folded == False:
+                i.requset_action()
+    
+    def get_river(self):
+        temp = []
+        for i in range(len(self.table_cards)):
+            if self.cards_flipped[i]:
+                temp.append(self.table_cards[i])
+        return temp
+          
+class Maestro:
+    # orecstrate all ui
+    class Page:
+        def __init__(self,name: str,desc: str,ops):
+            self.name = name # page title (string)
+            self.desc = desc # description (string)
+            self.ops = ops # user options ([[option name,function]])
+            
+    class Game(Page):
+        def setup_prompts(self,house: Mister_House):
+            s_money = self.get_int_from_player("How much money should players start with?\n","Thats not a valid integer. Try again")            
+            h_money = self.get_int_from_player("How much money should the house start with?\n","Thats not a valid integer. Try again")            
+            peep_n = self.get_int_from_player("How many People are playing?\n","Thats not a valid integer. Try again")
+            computer_n = self.get_int_from_player("How many Computers are playing?\n","Thats not a valid integer. Try again")
+            
+            users = []
+            for i in range(peep_n):
+                users.append(house.user("Player "+str(i),s_money))
+                
+            comps = []
+            for i in range(computer_n):
+                users.append(house.agent("Computer "+str(i),s_money))
+                
+            players = random.shuffle(users + comps)
+            
+            house.setup(h_money,players)
+                                    
+        def get_int_from_player(self,request: str,fail: str):
+            while(True):
+                try:
+                    return int(input(request))
+                    break
+                except:
+                    print(fail)
+                    continue
+        def get_move_from_player(self,house: Mister_House,player):
+            os.system('clr')
+            print("River:\n\n")
+            river_str = ""
+            temp = house.get_river
+            for i in house.get_river:
+                pass # get type and string and add it to the master string with spacing
+            
+            # print that big string out
+            # ask player for his move
+            #differuiniate intial move and in play
+            
+            
+    def __init__(self):
+        self.uni_cmds = { # commands usable on all screens
+            "help":self.help,
+            "quit":exit
+        }
+        self.Main = self.Page("Main Menu","The starting menu",{ # starting page
+            "play":[self.new_page,self.game_start],
+            "statistics":[self.new_page,self.stats]})
+        self.stats = self.Page("Statistics","Play Stats",{ # stats page
+            "back":[self.new_page,self.Main]})
+        self.game_start = self.Game("Setup","Declare starting money and ai count",{}) # game holding page
+        self.active_page = self.Main # set the initial page to the main menu
         
         # define cards and suits
         self.TYPE = [
@@ -122,44 +181,38 @@ class Mister_House:
             "diamond",
             "spade"] 
     
-    def setup(self,bank_value,init_cash,players):
-        self.players = players
-        self.bank = bank_value
-        self.max_turn = len(players) -1
-
-        # set player initial cashes
-        for i in self.players:
-            i.money = init_cash
-    
-    def draw():
-    #draw top card
-        temp = self.deck[self.deck_place]
-        self.deck_place+=1
-        return temp
-
-    def create_deck():
-    # create an empty deck
-        deck = [] 
-        # iterate through each card
-        for i in range(len(self.SUIT)):
-            for j in range(len(self.TYPE)):
-                deck[i*14+j] = [i,j]
-        # give it out
-        return random.shuffle(deck)
-        
-    def deal():
-        # shuffle
-        self.deck = create_deck()
-        # dealt to players
-        for i in self.players:
-            i.hand = [self.draw(),self.draw()]
-        # placed on table
-        for i in self.table_cards:
-            i = self.draw()
-        # flip first three
-        self.cards_flipped = [True,True,True,False,False]
-    
-    def all_turn(self):
-        for i in self.players:
-            if i.folded == False:
-                i.requset_action()
+    def new_page(self,move_to: Page): #load new page and get next move
+        os.system('clr')
+        self.active_page = move_to
+        print(move_to.name+"\n\n\n\n")
+        print("Options\n\n")
+        for i in move_to.ops:
+            print(str.capitalize(i)+" | ",end="")
+        if type(move_to) == self.Page:
+            while(True):
+                nextm = str.lower(input("Select an option:\n"))
+                try:
+                    temp = move_to.ops[nextm]
+                    temp[0](temp[1])
+                    break
+                except:
+                    try:
+                        if nextm == 'help':
+                            self.uni_cmds[nextm](move_to)
+                            break
+                        else:
+                                self.uni_cmds[nextm]()
+                                break
+                    except:
+                        print("Invalid Option, Try Again\n\n")
+                        continue
+            else:
+                self.play_game()
+                    
+        def help(self,page): # print info of current page
+            print("NAME:  "+page.name+"\n")
+            print("DESCRIPTION:  "+page.desc+"\n")
+            print("Options\n")
+            for i in move_to.ops:
+                print(str.capitalize(i)+" | ",end="")
+            print("\n\n\n")
