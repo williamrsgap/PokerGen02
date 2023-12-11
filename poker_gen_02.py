44db79e24c5e637d50659732dc8025755f975b9d
williamrsgap <146869255+williamrsgap@users.noreply.github.com>
2023-12-11 02:36:52 -0500

    Added proper flop functions
    Some don't quite work but the game is now in a semi-playable state (very buggy)

--- poker_gen_02.py
+++ poker_gen_02.py
@@ -26,6 +26,7 @@ class Mister_House:
             # Implement this method to read individual cards
             print(f"{self.name} reads the card: {card}")
 
+            #Unused
         def card_probability(self, house, target_card):
             # Calculate the probability of a specific card being on the river next
             remaining_cards = 52 - house.deck_place
@@ -33,47 +34,20 @@ class Mister_House:
             probability = target_count / remaining_cards
             return probability
         
-        def update_hand_with_river(self, river_cards):
-            # Update the player's hand with the current river cards
-            self.river_cards = river_cards
-        
-    class agent(Player):
-        #ai
-        def request_action(self,house):
-            if not self.folded:
-                # Check the strength of the hand and make decisions
-                hand_strength = self.evaluate_hand(house.turn)
-
-                if house.bet == 0:
-                    # No bets made yet, can check or bet
-                    if hand_strength >= 0.3:  
-                        self.up_bet(house, random.randint(10, 50))
-                        print(f"{self.name} bets.")
-                    else:
-                        print(f"{self.name} checks.")
-                else:
-                    # Bets have been made, can call, raise, or fold
-                    if hand_strength >= 0.4:  
-                        self.up_bet(house, random.randint(10, 50))
-                        print(f"{self.name} raises.")
-                    elif 0.2 <= hand_strength < 0.7:
-                        self.same_bet(house)
-                        print(f"{self.name} calls.")
-                    else:
-                        self.quitter()
-                        print(f"{self.name} folds.")
-            
         def evaluate_hand(self, turn):
-            print("Player Hand 0:", self.hand[0])
-            print("Player Hand 1:", self.hand[1])
-            sorted_hand = sorted(self.hand[0] + self.hand[1], key=lambda card: card[1], reverse=True)
-            
+            sorted_hand = sorted(self.hand, key=lambda index: index[1], reverse=True)
+
+            if not sorted_hand:  # Handle empty hand
+                return 0.0
+
             if turn == 0:
                 # Initial 2-card hand evaluation
                 return self.evaluate_initial_hand(sorted_hand)
             else:
                 # Evaluate hand considering the river cards
+                print("Test River", self.river_cards)
                 return self.evaluate_with_river(sorted_hand, self.river_cards, turn)
+          
 
         # Functions to check specific hand types
         def has_straight_flush(self, hand):
@@ -104,23 +78,26 @@ class Mister_House:
             # Check for same suit, pair, both high cards, or close numbers
             suits = set(card[0] for card in hand)
             if len(suits) == 1:  # Same suit
-                return 0.8
+                return 0.4
             elif any(hand.count(card) == 2 for card in hand):  # Pair
-                return 0.7
+                return 0.4
             elif all(card[1] >= 10 for card in hand):  # Both high cards
-                return 0.6
+                return 0.3
             elif abs(hand[0][1] - hand[1][1]) <= 1:  # Close numbers
-                return 0.5
+                return 0.25
             else:
-                return 0.4  # Other cases    
+                return 0.2  # Other cases    
         
         def evaluate_with_river(self, hand, river_cards, turn):
             # Evaluate hand based on the stage of the river
             if turn == 1:
+                print("Eval Flop")
                 return self.evaluate_flop(hand, river_cards)
             elif turn == 2:
+                print("Eval Turn")
                 return self.evaluate_turn(hand, river_cards)
             elif turn == 3:
+                print("Eval River")
                 return self.evaluate_river(hand, river_cards)
 
         def evaluate_flop(self, hand, river_cards):
@@ -147,55 +124,118 @@ class Mister_House:
             if self.has_straight_flush(hand + river_cards):
                 return 1.0
             elif self.has_four_of_a_kind(hand + river_cards):
-                return 0.9
+                return 0.95
             elif self.has_full_house(hand + river_cards):
-                return 0.8
+                return 0.90
             elif self.has_flush(hand + river_cards):
-                return 0.7
+                return 0.75
             elif self.has_straight(hand + river_cards):
-                return 0.6
+                return 0.65
             elif self.has_three_of_a_kind(hand + river_cards):
                 return 0.5
             elif self.has_two_pair(hand + river_cards):
-                return 0.4
+                return 0.35
             elif self.has_pair(hand + river_cards):
-                return 0.3
+                return 0.25
             else:
-                return 0.2  # High card
+                return 0.1  # High card
 
         def evaluate_river(self, hand, river_cards):
             if self.has_straight_flush(hand + river_cards):
                 return 1.0
             elif self.has_four_of_a_kind(hand + river_cards):
-                return 0.9
+                return 0.95
             elif self.has_full_house(hand + river_cards):
-                return 0.8
+                return 0.75
             elif self.has_flush(hand + river_cards):
-                return 0.7
+                return 0.65
             elif self.has_straight(hand + river_cards):
-                return 0.6
+                return 0.55
             elif self.has_three_of_a_kind(hand + river_cards):
-                return 0.5
+                return 0.45
             elif self.has_two_pair(hand + river_cards):
-                return 0.4
-            elif self.has_pair(hand + river_cards):
                 return 0.3
+            elif self.has_pair(hand + river_cards):
+                return 0.2
             else:
-                return 0.2  # High card
+                return 0.1  # High card
+        
+    class agent(Player):
+        #ai
+        def request_action(self,house):
+            if not self.folded:
+                # Check the strength of the hand and make decisions
+                hand_strength = self.evaluate_hand(house.turn)
+
+                if house.bet == 0:
+                    # No bets made yet, can check or bet
+                    if hand_strength >= 0.3:  
+                        self.up_bet(house, random.randint(10, 50))
+                        print(f"{self.name} bets.")
+                    else:
+                        print(f"{self.name} checks.")
+                else:
+                    # Bets have been made, can call, raise, or fold
+                    if hand_strength >= 0.4:  
+                        self.up_bet(house, random.randint(10, 50))
+                        print(f"{self.name} raises.")
+                    elif 0.2 <= hand_strength < 0.7:
+                        self.same_bet(house)
+                        print(f"{self.name} calls.")
+                    else:
+                        self.quitter()
+                        print(f"{self.name} folds.")
             
     class user(Player):
         #user   
-        def request_action(self,house):
+        def request_action(self, house):  # Add maestro as a parameter
             if house.bet == 0:
                 self.req_init_bet(house)
             else:
                 self.req_play(house)
-        def req_init_bet(self,house):
-            #ask the player for an initial bet through Maestro Class
-            pass
-        def req_play(self,house):
-            #ask the player to raise, call, or fold
-            pass
+        
+        def req_init_bet(self, house):
+            # Ask the player for an initial bet through Maestro Class
+            bet_amount = self.get_int_from_player(f"{self.name}, enter your initial bet: ", "Invalid input. Please enter a valid integer.")
+            self.up_bet(house, bet_amount)
+        
+        def req_play(self, house):
+            # Ask the player to raise, call, or fold
+            while True:
+                action = input(f"{self.name}, choose your action (raise/call/fold): ").lower()
+                if action in ['raise', 'call', 'fold']:
+                    if action == 'raise':
+                        raise_amount = self.get_int_from_player("Enter the raise amount: ", "Invalid input. Please enter a valid integer.")
+                        self.up_bet(house, raise_amount)
+                    elif action == 'call':
+                        self.same_bet(house)
+                    elif action == 'fold':
+                        self.quitter()
+                    break
+                else:
+                    print("Invalid choice. Please choose raise, call, or fold.")
+        
+        def get_int_from_player(self,request: str,fail: str):
+            while(True):
+                try:
+                    return int(input(request))
+                    break
+                except:
+                    print(fail)
+                    continue
+        
+        def evaluate_hand(self, turn):
+            sorted_hand = sorted(self.hand, key=lambda index: index[1], reverse=True)
+
+            if not sorted_hand:  # Handle empty hand
+                return 0.0
+
+            if turn == 0:
+                # Initial 2-card hand evaluation
+                return self.evaluate_initial_hand(sorted_hand)
+            else:
+                # Evaluate hand considering the river cards
+                return self.evaluate_with_river(sorted_hand, self.river_cards, turn)
           
     def __init__(self):
         self.players = []  # all player
@@ -240,16 +280,26 @@ class Mister_House:
         # dealt to players
         for i in self.players:
             i.hand = [self.draw(),self.draw()]
-        # placed on table
-        for i in range(len(self.table_cards)):
-            self.table_cards[i] = self.draw()
-        # flip first three
-        self.cards_flipped = [True,True,True,False,False]
     
-    def all_turn(self):
+    def deal_river(self, turn):   
+        # flip first three
+        if turn == 1:
+            for i in range(3):
+                self.table_cards[i] = self.draw()
+            self.river_cards = self.table_cards.copy()
+        elif turn == 2:
+            self.table_cards[3] = self.draw()
+            self.river_cards.append(self.table_cards[3]) # 4 cards for the turn
+            print(self.table_cards[:4])
+        elif turn == 3:
+            self.table_cards[4] = self.draw()
+            self.river_cards.append(self.table_cards[4])  # All 5 cards for the river
+            print(self.table_cards[:5])
+    
+    def all_turn(self, maestro):  # Add maestro as a parameter
         for i in self.players:
             if i.folded == False:
-                i.request_action()
+                i.request_action(self, maestro)  # Pass maestro instance
     
     def get_river(self):
         temp = []
@@ -267,10 +317,10 @@ class Mister_House:
 class Maestro:
     # orecstrate all ui
     class Page:
-        def __init__(self,name: str,desc: str,ops):
+        def __init__(self,name: str,desc: str,options):
             self.name = name # page title (string)
             self.desc = desc # description (string)
-            self.ops = ops # user options ([[option name,function]])
+            self.options = options # user options ([[option name,function]])
             
     class Game(Page):
         def setup_prompts(self,house: Mister_House):
@@ -291,14 +341,6 @@ class Maestro:
             
             house.setup(h_money,players)
                                     
-        def get_int_from_player(self,request: str,fail: str):
-            while(True):
-                try:
-                    return int(input(request))
-                    break
-                except:
-                    print(fail)
-                    continue
         def get_move_from_player(self,house: Mister_House,player):
             os.system('clr')
             print("River:\n\n")
@@ -314,16 +356,8 @@ class Maestro:
             
     def __init__(self):
         self.uni_cmds = { # commands usable on all screens
-            "help":self.help,
             "quit":exit
         }
-        self.Main = self.Page("Main Menu","The starting menu",{ # starting page
-            "play":[self.new_page,self.game_start],
-            "statistics":[self.new_page,self.stats]})
-        self.stats = self.Page("Statistics","Play Stats",{ # stats page
-            "back":[self.new_page,self.Main]})
-        self.game_start = self.Game("Setup","Declare starting money and ai count",{}) # game holding page
-        self.active_page = self.Main # set the initial page to the main menu
         
         # define cards and suits
         self.TYPE = [
@@ -340,83 +374,81 @@ class Maestro:
             "queen",
             "king",
             "ace"]
+        
         self.SUIT=[
             "heart",
             "club",
             "diamond",
             "spade"] 
-    
-    def new_page(self,move_to: Page): #load new page and get next move
+        
+    def stats(self, house):
         os.system('clr')
-        self.active_page = move_to
-        print(move_to.name+"\n\n\n\n")
-        print("Options\n\n")
-        for i in move_to.ops:
-            print(str.capitalize(i)+" | ",end="")
-        if type(move_to) == self.Page:
-            while(True):
-                nextm = str.lower(input("Select an option:\n"))
-                try:
-                    temp = move_to.ops[nextm]
-                    temp[0](temp[1])
-                    break
-                except:
-                    try:
-                        if nextm == 'help':
-                            self.uni_cmds[nextm](move_to)
-                            break
-                        else:
-                                self.uni_cmds[nextm]()
-                                break
-                    except:
-                        print("Invalid Option, Try Again\n\n")
-                        continue
-            else:
-                self.play_game()
+        print("\nPlayer Statistics:")
+        for player in house.players:
+            print(f"{player.name} - Bets: {player.bets}, Folds: {player.folds}, All-ins: {player.all_ins}, Total Betted: {player.total_bet}")
+        input("\nPress Enter to go back to the main menu.")
+
+    
+    def play_game(self):
+        while True:
+            os.system('clr')
+            print("Main Menu:")
+            print("1. Stats")
+            print("2. Quit")
+            print("3. Play Game")
+
+            choice = input("Select an option (1/2/3): ")
+
+            if choice == '1':
+                self.stats(house)
+            elif choice == '2':
+                exit()
+            elif choice == '3':
+                house = Mister_House()
+                player_name = input("Enter your name: ")
+                user = Mister_House.user(player_name, 5000)
+                ai_count = int(input("Enter the number of AI players: "))
+                ai_names = ["AI Bot " + str(i) for i in range(ai_count)]
+                ais = [Mister_House.agent(ai_name, 5000) for ai_name in ai_names]
+                players = [user] + ais
+                house.setup(5000, players)
+                house.deal()
+
+                # Simulate a few turns (replace this with your actual game logic)
+                for turn in range(4):  # Assuming four turns for this example
+                    print(f"\n--- Turn {turn + 1} ---")
                     
-        def help(self,page): # print info of current page
-            print("NAME:  "+page.name+"\n")
-            print("DESCRIPTION:  "+page.desc+"\n")
-            print("Options\n")
-            for i in move_to.ops:
-                print(str.capitalize(i)+" | ",end="")
-            print("\n\n\n")
+                    if turn >= 1:
+                        house.deal_river(turn)
+
+                    # User's turn
+                    user.request_action(house)
+                    user_hand_strength = user.evaluate_hand(turn)
+                    print(f"{user.name}'s hand strength: {user_hand_strength}")
+                    print(f"{user.name}'s hand: {user.hand}")
+
+                    # AI turns
+                    for ai in ais:
+                        print(f"{ai.name}'s hand: {ai.hand}")
+                        ai.request_action(house)
+                        agent_hand_strength = ai.evaluate_hand(turn)
+                        print(f"{ai.name}'s hand strength: {agent_hand_strength}")
+                        print(f"{ai.name}'s hand: {ai.hand}")
+                # Evaluate final hands
+                print(f"River cards: {house.table_cards}")
+                
+                user_final_strength = user.evaluate_hand(house.turn)
+                agent_final_strength = ais[0].evaluate_hand(house.turn)
+
+                # Display final hand strengths
+                print(f"\nFinal hand strength for {user.name}: {user_final_strength}")
+                print(f"Final hand strength for {ais[0].name}: {agent_final_strength}")
+
+                input("\nPress Enter to go back to the main menu.")
+            else:
+                print("Invalid choice. Please try again.")
             
-# Test code
+maestro = Maestro()
 
-# Create an instance of Mister_House
-house = Mister_House()
-
-# Create players (1 user and 1 agent for simplicity)
-user = Mister_House.user("John Doe", 1000)
-agent = Mister_House.agent("AI Bot", 1000)
-
-# Setup the game
-house.setup(5000, [user, agent])
-
-# Deal initial cards
-house.deal()
-
-# Simulate a few turns (replace this with your actual game logic)
-for turn in range(4):  # Assuming four turns for this example
-    print(f"\n--- Turn {turn + 1} ---")
-
-    # User's turn
-    user.request_action(house)
-    print(f"{user.name}'s hand: {user.hand}")
-
-    # Agent's turn
-    print(f"{agent.name}'s hand: {agent.hand}")
-    agent.request_action(house)
-    print(f"{agent.name}'s hand: {agent.hand}")
-
-    # Display river cards
-    print(f"River cards: {house.table_cards}")
-
-# Evaluate final hands
-user_final_strength = user.evaluate_hand(house.turn)
-agent_final_strength = agent.evaluate_hand(house.turn)
-
-# Display final hand strengths
-print(f"\nFinal hand strength for {user.name}: {user_final_strength}")
-print(f"Final hand strength for {agent.name}: {agent_final_strength}")
+while True:
+    maestro.play_game()
